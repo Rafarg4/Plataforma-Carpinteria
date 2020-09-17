bf2981e36239c35caeed196ef6207b5c70ef6ec6
Giovani <gio.gimenez@gmail.com>
2020-09-16 21:11:49 -0400

    Modificacion del home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,7 @@
 	  <h1 class="display-4">Hola, {{ Auth::user()->name}}! usted pertenece al grupo de Red Team</h1>
 	  <p>Si visualiza esta pantalla de bienvenida esta habilitado para programar.</p>
       <p>commit Ale</p>
+	  <p>commit Gio</p>
 	  <a class="btn btn-primary btn-lg" href="#" role="button">Iniciar</a>
 </div>
 
