fb3f62283899a96b36d13ab722badf137510f961
unknown <rep141998@gmail.com>
2020-09-19 18:06:07 -0400

    datatable

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -157,34 +157,38 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/icheck.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/js/select2.min.js"></script>
+     <!-- Datatables -->
+     <link href="{{ asset('css/datatable.css') }}"  rel="stylesheet">
+     <script type="text/javascript" src="{{ asset('js/datatable.js') }}"></script>
+     <link href="{{ asset('css/buttons.css') }}"  rel="stylesheet">
 
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.6.2/css/buttons.dataTables.min.css"> 
-    <script src="https://cdn.datatables.net/buttons/1.6.2/js/dataTables.buttons.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-    <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.6.2/js/dataTables.buttons.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.flash.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.html5.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.print.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
+     <script type="text/javascript" src="{{ asset('js/buttons.js') }}"></script>
+
+     <script type="text/javascript" src="{{ asset('js/flashexport.js') }}"></script>
+
+     <script type="text/javascript" src="{{ asset('js/jszip.js') }}"></script>
+
+     <script type="text/javascript" src="{{ asset('js/pdfmake.js') }}"></script>
+
+     <script type="text/javascript" src="{{ asset('js/pdffont.js') }}"></script>
+
+     <script type="text/javascript" src="{{ asset('js/html5.js') }}"></script>
+
+     <script type="text/javascript" src="{{ asset('js/print.js') }}"></script>
 
     @yield('scripts')
      <script>
-     $(document).ready(function() {
-        $('#data-table').DataTable( {
-            "language": {
-                "url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"
-            },
-            dom: 'Bfrtip',
-            buttons: [
-                'copy', 'csv', 'excel', 'pdf', 'print'
-            ]
-        } );
+$(document).ready(function() {
+    $('#data-table').DataTable( {
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"
+        },
+        dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ]
     } );
-
+} );
         </script>
     @stack('scripts')
 </body>
