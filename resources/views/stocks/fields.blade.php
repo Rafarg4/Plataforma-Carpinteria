5c403623d3dddfd680f02edcd7533571fd0f8ec4
Francis <francy.alcarodri@gmail.com>
2020-09-19 21:58:04 -0400

    Correciones Vistas V.2

--- resources/views/stocks/fields.blade.php
+++ resources/views/stocks/fields.blade.php
@@ -1,23 +1,23 @@
 <!-- Cantidad Field -->
 <div class="form-group col-sm-6">
-    {!! Form::label('cantidad', 'Cantidad:') !!}
+    {!! Form::label('cantidad', 'CANTIDAD:') !!}
     {!! Form::number('cantidad', null, ['class' => 'form-control']) !!}
 </div>
 
 <!-- Producto Field -->
 <div class="form-group col-sm-6">
-    {!! Form::label('producto', 'Producto:') !!}
+    {!! Form::label('producto', 'PRODUCTO:') !!}
      {!! Form::select('producto',array('Sin eleccion' => 'Sin eleccion','tornillos' => 'tornillos', 'madera' => 'madera','clavos' => 'clavos'),null, ['class' => 'form-control' ])!!}
 </div>
 
 <!-- Deposito Field -->
 <div class="form-group col-sm-6">
-    {!! Form::label('deposito', 'Deposito:') !!}
+    {!! Form::label('deposito', 'DEPOSITO:') !!}
     {!! Form::select('deposito',array('Sin eleccion' => 'Sin eleccion','compras' => 'compras', 'ventas' => 'ventas','produccion' => 'produccion'),null, ['class' => 'form-control' ])!!}
 </div>
 
 <!-- Submit Field -->
 <div class="form-group col-sm-12">
-    {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
-    <a href="{{ route('stocks.index') }}" class="btn btn-default">Cancel</a>
+    {!! Form::submit('GUARDAR', ['class' => 'btn btn-primary']) !!}
+    <a href="{{ route('stocks.index') }}" class="btn btn-default">CANCELAR</a>
 </div>
--- resources/views/stocks/show_fields.blade.php
+++ resources/views/stocks/show_fields.blade.php
@@ -1,30 +1,30 @@
 <!-- Cantidad Field -->
 <div class="form-group col-sm-6">
-    {!! Form::label('cantidad', 'Cantidad:') !!}
+    {!! Form::label('cantidad', 'CANTIDAD:') !!}
     <p>{{ $stock->cantidad }}</p>
 </div>
 
 <!-- Producto Field -->
 <div class="form-group col-sm-6">
-    {!! Form::label('producto', 'Producto:') !!}
+    {!! Form::label('producto', 'PRODUCTO:') !!}
     <p>{{ $stock->producto }}</p>
 </div>
 
 <!-- Deposito Field -->
 <div class="form-group col-sm-6">
-    {!! Form::label('deposito', 'Deposito:') !!}
+    {!! Form::label('deposito', 'DEPOSITO:') !!}
     <p>{{ $stock->deposito }}</p>
 </div>
 
 <!-- Created At Field -->
 <div class="form-group col-sm-6">
-    {!! Form::label('created_at', 'Created At:') !!}
+    {!! Form::label('created_at', 'CREADO:') !!}
     <p>{{ $stock->created_at }}</p>
 </div>
 
 <!-- Updated At Field -->
 <div class="form-group col-sm-6">
-    {!! Form::label('updated_at', 'Updated At:') !!}
+    {!! Form::label('updated_at', 'ACTUALIZADO:') !!}
     <p>{{ $stock->updated_at }}</p>
 </div>
 
