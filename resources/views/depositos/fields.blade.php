04b77511aca8fd67a0176f21fb0eb9428b4aa2e8
Clara <clara.lopezguerrero97@gmail.com>
2020-09-26 19:31:50 -0400

    Ediccion de vistas y estilo clara y valeria

--- resources/views/depositos/fields.blade.php
+++ resources/views/depositos/fields.blade.php
@@ -6,6 +6,6 @@
 
 <!-- Submit Field -->
 <div class="form-group col-sm-12">
-    {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
-    <a href="{{ route('depositos.index') }}" class="btn btn-default">Cancel</a>
+    {!! Form::submit('GUARDAR', ['class' => 'btn btn-primary']) !!}
+    <a href="{{ route('depositos.index') }}" class="btn btn-default">CANCELAR</a>
 </div>
--- resources/views/depositos/show.blade.php
+++ resources/views/depositos/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <section class="content-header">
         <h1>
-            Deposito
+            DEPOSITO
         </h1>
     </section>
     <div class="content">
@@ -11,7 +11,7 @@
             <div class="box-body">
                 <div class="row" style="padding-left: 20px">
                     @include('depositos.show_fields')
-                    <a href="{{ route('depositos.index') }}" class="btn btn-default">Back</a>
+                    <a href="{{ route('depositos.index') }}" class="btn btn-default">VOLVER</a>
                 </div>
             </div>
         </div>
