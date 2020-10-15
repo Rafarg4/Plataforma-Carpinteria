bfda51538e13fa2b355d62aa480f77eef7e3f87b
Manueltiesto <manulito82@gmail.com>
2020-10-14 21:42:16 -0300

    Edicion de Vistas v3 Valeria C

--- resources/views/stocks/fields.blade.php
+++ resources/views/stocks/fields.blade.php
@@ -1,23 +1,23 @@
 <!-- Deposito Id Field -->
 <div class="form-group col-sm-6">
-    {!! Form::label('deposito_id', 'Deposito Id:') !!}
+    {!! Form::label('deposito_id', 'DEPOSITO ID:') !!}
     {!! Form::select('deposito_id', $depositos, null, ['class' => 'form-control']) !!}
 </div>
 
 <!-- Producto Field -->
 <div class="form-group col-sm-6">
-    {!! Form::label('producto', 'Producto:') !!}
+    {!! Form::label('producto', 'PRODUCTO:') !!}
     {!! Form::text('producto', null, ['class' => 'form-control']) !!}
 </div>
 
 <!-- Cantidad Field -->
 <div class="form-group col-sm-6">
-    {!! Form::label('cantidad', 'Cantidad:') !!}
+    {!! Form::label('cantidad', 'CANTIDAD:') !!}
     {!! Form::number('cantidad', null, ['class' => 'form-control']) !!}
 </div>
 
 <!-- Submit Field -->
 <div class="form-group col-sm-12">
-    {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
-    <a href="{{ route('stocks.index') }}" class="btn btn-default">Cancel</a>
+    {!! Form::submit('Guardar', ['class' => 'btn btn-primary']) !!}
+    <a href="{{ route('stocks.index') }}" class="btn btn-default">CANCELAR</a>
 </div>
--- resources/views/stocks/table.blade.php
+++ resources/views/stocks/table.blade.php
@@ -2,10 +2,10 @@
     <table class="table" id="data-table">
         <thead>
             <tr>
-                <th>Deposito Id</th>
-        <th>Producto</th>
-        <th>Cantidad</th>
-                <th>Action</th>
+                <th>DEPOSITO ID</th>
+        <th>PRODUCTO</th>
+        <th>CANTIDAD</th>
+                <th>ACCION</th>
             </tr>
         </thead>
         <tbody>
@@ -15,11 +15,12 @@
             <td>{{ $stock->producto }}</td>
             <td>{{ $stock->cantidad }}</td>
                 <td>
-                    {!! Form::open(['route' => ['stocks.destroy', $stock->id], 'method' => 'delete']) !!}
+                    {!! Form::open(['rout
+                    e' => ['stocks.destroy', $stock->id], 'method' => 'delete']) !!}
                     <div class='btn-group'>
                         <a href="{{ route('stocks.show', [$stock->id]) }}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
                         <a href="{{ route('stocks.edit', [$stock->id]) }}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
-                        {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
+                        {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('ESTÁ SEGURO ?')"]) !!}
                     </div>
                     {!! Form::close() !!}
                 </td>
